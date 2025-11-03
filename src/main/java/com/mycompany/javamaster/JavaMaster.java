278153579df33761d419ce0ee1127b16edba7788
Avila-Pato <p.avilaf1998@gmail.com>
2025-11-02 21:21:53 -0300

    Subiendo curso

---
+++ src/main/java/com/mycompany/javamaster/JavaMaster.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.javamaster;
+
+
+public class JavaMaster {
+
+    public static void main(String[] args) {
+//  EJERCICIO1 LALMADA TIPO VOID
+      IntercambiarVariable ejercicio1 = new IntercambiarVariable();
+      ejercicio1.intercambiar();
+//  EJERCICIO2 LLAMADA TIPO STRING
+//      EntradasDatos entrada = new EntradasDatos();
+//        System.out.println(entrada.entradaDeDatos() );
+//  EXPRESIONES ARITMETICAS LLAMDA TIPO STRING
+       ExpresionesAritmeticas expresiones = new  ExpresionesAritmeticas();
+        System.out.println(expresiones.expresionesAritmeticas());
+    }
+}
