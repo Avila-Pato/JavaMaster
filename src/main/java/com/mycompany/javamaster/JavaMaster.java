0662e6b1a1a7547b0ccf7e3b62c04844235b7937
Avila-Pato <p.avilaf1998@gmail.com>
2025-11-03 23:19:06 -0300

    Aggreggando Operacion con Mathc y operaciones con interfaces no he suado jFrame

--- src/main/java/com/mycompany/javamaster/JavaMaster.java
+++ src/main/java/com/mycompany/javamaster/JavaMaster.java
@@ -6,13 +6,20 @@ public class JavaMaster {
 
     public static void main(String[] args) {
 //  EJERCICIO1 LALMADA TIPO VOID
-      IntercambiarVariable ejercicio1 = new IntercambiarVariable();
-      ejercicio1.intercambiar();
+//      IntercambiarVariable ejercicio1 = new IntercambiarVariable();
+//      ejercicio1.intercambiar();
 //  EJERCICIO2 LLAMADA TIPO STRING
 //      EntradasDatos entrada = new EntradasDatos();
 //        System.out.println(entrada.entradaDeDatos() );
 //  EXPRESIONES ARITMETICAS LLAMDA TIPO STRING
-       ExpresionesAritmeticas expresiones = new  ExpresionesAritmeticas();
-        System.out.println(expresiones.expresionesAritmeticas());
+//       ExpresionesAritmeticas expresiones = new  ExpresionesAritmeticas();
+//        System.out.println(expresiones.expresionesAritmeticas());
+//  INTERFACES GRAFICAS sin JFRAM  
+//        InterfacesGraficas ventana = new InterfacesGraficas();
+//        ventana.interfaz();
+//  OPERACIONES MATIMATICAS CON MATH // !!TIPO STRING POR ESO LA LLAMADA SE LLAMA POR CONSOLA
+        OperacionesMath Math = new OperacionesMath();
+        System.out.println("LLamada Math \n" + Math.MathOperations());
+
     }
 }
